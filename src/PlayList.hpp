e20f4df40e374d79de521c76c6b8aa1b36d67c8b
Volkan Orhan <contactme@volkanorhan.com>
2022-10-10 16:13:35 +0300

    Player and PlayList interface have been updated for open, metadata and play methods. Comments has been added for PlayList interface. Player interface's comments will be added later.

--- src/PlayList.hpp
+++ src/PlayList.hpp
@@ -16,23 +16,102 @@ You should have received a copy of the GNU Lesser General Public License along w
 namespace ModPlugPlayer {
     class PlayList {
     public:
+        /*!
+         * \brief getCurrentItem gets currently loaded item of the playlist.
+         * \return currently loaded item of the playlist.
+         */
         virtual PlayListItem getCurrentItem() = 0;
     signals:
+        /*!
+         * \brief requestMetaData is expected to be emitted when an item is added to playlist or emitted for each item when playlist is loaded from file.
+         * \param playListItem
+         */
+        virtual void requestMetaData(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief open is expected to be emitted by onNextSong or onPreviousSong when next or previous item is requested.
+         * \param playListItem
+         */
+        virtual void open(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief play is expected to be emitted when an item is double clicked or an item's play button is pressed
+         * \param playListItem
+         */
         virtual void play(const PlayListItem playListItem) = 0;
-        virtual void pause() = 0;
-        virtual void resume() = 0;
-        virtual void stop() = 0;
-        virtual void next() = 0;
-        virtual void previous() = 0;
-        virtual void clear() = 0;
+
+        /*!
+         * \brief pause is expected to be emitted when pause button of an playlist item is pressed.
+         */
+        virtual void pause(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief resume is expected to be emitted when resume button of an playlist item is pressed.
+         */
+        virtual void resume(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief stop is expected to be emitted when the playlist needs the player to stop the song.
+         * This signal is not used currently since there is not any stop button on playlist items.
+         */
+        virtual void stop(const PlayListItem playListItem) = 0;
+
     public slots:
+        /*!
+         * \brief onMetaData
+         * \param playListItem
+         */
+        virtual void onMetaData(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onOpen is expected to be triggered when the player opens the corresponding playlist item.
+         * Expected to make the corresponding playlist item on the playlist is shown as open (eg. showing it in bold fonts, etc.),
+         * and to make the previously opened item of the playlist unopened.
+         * \param playListItem
+         */
+        virtual void onOpen(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onPlay is expected to be triggered when the player plays the corresponding playlist item.
+         * Expected to make the corresponding playlist item on the playlist is shown as open and playing
+         * (eg. showing it in bold fonts, making its icon as playing, etc.), and to make the previously opened item of the playlist unopened.
+         * \param playListItem
+         */
         virtual void onPlay(const PlayListItem playListItem) = 0;
-        virtual void onPlay() = 0;
-        virtual void onPause() = 0;
-        virtual void onResume() = 0;
-        virtual void onStop() = 0;
+
+        /*!
+         * \brief onPause is expected to be triggered when the player pauses the corresponding playlist item.
+         * Expected to make the corresponding playlist item on the playlist is shown as paused.
+         */
+        virtual void onPause(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onResume is expected to be triggered when the player resumes the corresponding playlist item.
+         * Expected to make the corresponding playlist item on the playlist is shown as playing.
+         */
+        virtual void onResume(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onStop is expected to be triggered when the player stops the corresponding playlist item.
+         * Expected to make the corresponding playlist item on the playlist is shown as stopped.
+         */
+        virtual void onStop(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onNextSong is expected to make the playlist to emit open signal with the next playlist item.
+         */
         virtual void onNextSong() = 0;
+
+        /*!
+         * \brief onPreviousSong is expected to make the playlist to emit open signal with the previous playlist item.
+         */
         virtual void onPreviousSong() = 0;
+
+        /*!
+         * \brief onClear is expected to clear the playlist, and to make the player stop and unload the current song.
+         */
         virtual void onClear() = 0;
     };
 }
+
+Q_DECLARE_INTERFACE(ModPlugPlayer::PlayList, "PlayList")
--- src/Player.hpp
+++ src/Player.hpp
@@ -18,45 +18,241 @@ You should have received a copy of the GNU General Public License along with thi
 namespace ModPlugPlayer {
     class Player {
     signals:
+        /*!
+         * \brief open is expected to be emitted when the player opens the corresponding file.
+         * \param filePath path of the file to be opened.
+         */
         virtual void open(const std::filesystem::path filePath) = 0;
+
+        /*!
+         * \brief open is expected to be emitted when the player opens the corresponding playlist item.
+         * \param playListItem playlist item to be opened.
+         */
         virtual void open(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief stop
+         */
         virtual void stop() = 0;
+
+        /*!
+         * \brief stop
+         * \param playListItem
+         */
+        virtual void stop(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief play
+         */
         virtual void play() = 0;
+
+        /*!
+         * \brief play
+         * \param playListItem
+         */
+        virtual void play(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief pause
+         */
         virtual void pause() = 0;
+
+        /*!
+         * \brief pause
+         * \param playListItem
+         */
+        virtual void pause(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief resume
+         */
         virtual void resume() = 0;
+
+        /*!
+         * \brief resume
+         * \param playListItem
+         */
+        virtual void resume(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief previous
+         */
         virtual void previous() = 0;
+
+        /*!
+         * \brief next
+         * \param playListItem
+         */
         virtual void next() = 0;
+
+        /*!
+         * \brief changeVolume
+         * \param volume
+         */
         virtual void changeVolume(const int volume) = 0;
+
+        /*!
+         * \brief scrubTime
+         * \param position
+         */
         virtual void scrubTime(const int position) = 0;
+
+        /*!
+         * \brief changeRepeat
+         * \param repeat
+         */
         virtual void changeRepeat(const ModPlugPlayer::RepeatState repeat) = 0;
+
+        /*!
+         * \brief setAlwaysOnTop
+         * \param alwaysOnTop
+         */
         virtual void setAlwaysOnTop(const bool alwaysOnTop) = 0;
+
+        /*!
+         * \brief hideTitleBar
+         * \param hide
+         */
         virtual void hideTitleBar(const bool hide) = 0;
+
+        /*!
+         * \brief snapToViewPort
+         * \param toBeSnappedToViewPort
+         */
         virtual void snapToViewPort(const bool toBeSnappedToViewPort) = 0;
+
+        /*!
+         * \brief keepStayingInViewPort
+         * \param toBeKeptStayingInViewPort
+         */
         virtual void keepStayingInViewPort(const bool toBeKeptStayingInViewPort) = 0;
 
     public:
+        /*!
+         * \brief getVolume
+         * \return
+         */
         virtual int getVolume() const = 0;
+
+        /*!
+         * \brief isAlwaysOnTop
+         * \return
+         */
         virtual bool isAlwaysOnTop() const = 0;
+
+        /*!
+         * \brief isTitleBarHidden
+         * \return
+         */
         virtual bool isTitleBarHidden() const = 0;
+
+        /*!
+         * \brief isSnapToViewPort
+         * \return
+         */
         virtual bool isSnapToViewPort() const = 0;
+
+        /*!
+         * \brief isKeptStayingInViewPort
+         * \return
+         */
         virtual bool isKeptStayingInViewPort() const = 0;
 
     public slots:
+        /*!
+         * \brief onOpen
+         * \param filePath
+         */
         virtual void onOpen(const std::filesystem::path filePath) = 0;
+
+        /*!
+         * \brief onOpen
+         * \param playListItem
+         */
         virtual void onOpen(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onStop
+         */
         virtual void onStop() = 0;
+
+        /*!
+         * \brief onPlay
+         */
         virtual void onPlay() = 0;
+
+        /*!
+         * \brief onPlay
+         * \param playListItem
+         */
+        virtual void onPlay(const PlayListItem playListItem) = 0;
+
+        /*!
+         * \brief onPause
+         */
         virtual void onPause() = 0;
+
+        /*!
+         * \brief onResume
+         */
         virtual void onResume() = 0;
+
+        /*!
+         * \brief onPrevious
+         */
         virtual void onPrevious() = 0;
+
+        /*!
+         * \brief onNext
+         */
         virtual void onNext() = 0;
+
+        /*!
+         * \brief onChangeVolume
+         * \param volume
+         */
         virtual void onChangeVolume(const int volume) = 0;
+
+        /*!
+         * \brief onScrubTime
+         * \param position
+         */
         virtual void onScrubTime(const int position) = 0;
+
+        /*!
+         * \brief onChangeRepeat
+         * \param repeat
+         */
         virtual void onChangeRepeat(const ModPlugPlayer::RepeatState repeat) = 0;
+
+        /*!
+         * \brief onSetAlwaysOnTop
+         * \param alwaysOnTop
+         */
         virtual void onSetAlwaysOnTop(const bool alwaysOnTop) = 0;
+
+        /*!
+         * \brief onHideTitleBar
+         * \param hide
+         */
         virtual void onHideTitleBar(const bool hide) = 0;
+
+        /*!
+         * \brief onSetSnapToViewPort
+         * \param snapToViewPort
+         */
         virtual void onSetSnapToViewPort(const bool snapToViewPort) = 0;
+
+        /*!
+         * \brief onSetKeepStayingInViewPort
+         * \param keepStayingInViewPort
+         */
         virtual void onSetKeepStayingInViewPort(const bool keepStayingInViewPort) = 0;
+
+        /*!
+         * \brief onSetSnappingThreshold
+         * \param snappingThreshold
+         */
         virtual void onSetSnappingThreshold(const int snappingThreshold) = 0;
     };
 }
