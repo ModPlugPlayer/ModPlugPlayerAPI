89090b2b5d6bd5054da975fc6c3273d3dfeaa7e8
Volkan Orhan <contactme@volkanorhan.com>
2024-12-03 12:59:47 +0300

    AGC, XBass, Surround and Reverb state signals and signal handler declarations were added to Player interface.

--- src/Player.hpp
+++ src/Player.hpp
@@ -35,6 +35,10 @@ namespace ModPlugPlayer {
         virtual void timeScrubbingRequested(const int position) = 0;
         virtual void repeatModeChangeRequested(const ModPlugPlayer::RepeatMode repeatMode) = 0;
         virtual void eqStateChangeRequested(const bool activated) = 0;
+        virtual void agcStateChangeRequested(const bool activated) = 0;
+        virtual void xBassStateChangeRequested(const bool activated) = 0;
+        virtual void surroundStateChangeRequested(const bool activated) = 0;
+        virtual void reverbStateChangeRequested(const bool activated) = 0;
         virtual void interpolationModeChangeRequested(const ModPlugPlayer::InterpolationMode interpolationMode) = 0;
         virtual void alwaysOnTopStateChangeRequested(const bool alwaysOnTop) = 0;
         virtual void titleBarHidingStateChangeRequested(const bool hide) = 0;
@@ -58,6 +62,10 @@ namespace ModPlugPlayer {
         virtual void timeScrubbed(const int position) = 0;
         virtual void repeatModeChanged(const ModPlugPlayer::RepeatMode repeat) = 0;
         virtual void eqStateChanged(const bool activated) = 0;
+        virtual void agcStateChanged(const bool activated) = 0;
+        virtual void xBassStateChanged(const bool activated) = 0;
+        virtual void surroundStateChanged(const bool activated) = 0;
+        virtual void reverbStateChanged(const bool activated) = 0;
         virtual void interpolationModeChanged(const ModPlugPlayer::InterpolationMode interpolationMode) = 0;
         virtual void alwaysOnTopStateChanged(const bool alwaysOnTop) = 0;
         virtual void titleBarHidingStateChanged(const bool hide) = 0;
@@ -94,6 +102,10 @@ namespace ModPlugPlayer {
 
         virtual void onRepeatModeChangeRequested(const ModPlugPlayer::RepeatMode repeatMode) = 0;
         virtual void onEqStateChangeRequested(const bool activated) = 0;
+        virtual void onAGCStateChangeRequested(const bool activated) = 0;
+        virtual void onXBassStateChangeRequested(const bool activated) = 0;
+        virtual void onSurroundStateChangeRequested(const bool activated) = 0;
+        virtual void onReverbStateChangeRequested(const bool activated) = 0;
         virtual void onInterpolationModeChangeRequested(const ModPlugPlayer::InterpolationMode interpolationMode) = 0;
         virtual void onAlwaysOnTopStateChangeRequested(const bool alwaysOnTop) = 0;
         virtual void onTitleBarHidingStateChangeRequested(const bool hide) = 0;
