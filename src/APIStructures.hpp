d7c0a900e189c19af978728815c7af42a9809f06
Volkan Orhan <contactme@volkanorhan.com>
2026-05-11 00:30:59 +0300

    Move audio config and versioning API structs to Andromeda Signal Lab

--- src/APIStructures.hpp
+++ src/APIStructures.hpp
@@ -44,51 +44,6 @@ namespace ModPlugPlayer {
         Error
     };
 
-    enum class SamplingFrequency:int {
-        Hz8000=8000,
-        Hz9600=9600,
-        Hz11025=11025,
-        Hz12000=12000,
-        Hz16000=16000,
-        Hz22050=22050,
-        Hz24000=24000,
-        Hz32000=32000,
-        Hz44100=44100,
-        Hz48000=48000,
-        Hz88200=88200,
-        Hz96000=96000,
-        Hz192000=192000
-    };
-
-    enum class BitDepth:int {
-        Bits8 = 8,
-        Bits16 = 16,
-        Bits24 = 24,
-        Bits32 = 32
-    };
-
-    enum class ChannelMode:int {
-        Mono = 1,
-        Stereo = 2,
-        Quadraphonic = 4,
-        Pentaphonic = 5,
-        Surround_5_1 = 6,
-        Surround_7_1 = 8
-    };
-
-    enum class SampleDataFormat {
-        Float32 = 320,
-        Int32 = 32,
-        Int16 = 16
-    };
-
-    struct SoundResolution {
-        BitDepth bitDepth = BitDepth::Bits16;
-        SamplingFrequency sampleRate = SamplingFrequency::Hz44100;
-        ChannelMode channelMode = ChannelMode::Stereo;
-        SampleDataFormat sampleDataFormat = SampleDataFormat::Float32;
-    };
-
     enum class RepeatMode:int {
         NoRepeat = 0,
         RepeatSong = 1,
@@ -98,18 +53,18 @@ namespace ModPlugPlayer {
 
     inline RepeatMode& operator++(RepeatMode& state, int) {
         switch(state) {
-        case RepeatMode::NoRepeat:
-            state = RepeatMode::RepeatSong;
-            break;
-        case RepeatMode::RepeatSong:
-            state = RepeatMode::LoopSong;
-            break;
-        case RepeatMode::LoopSong:
-            state = RepeatMode::RepeatPlayList;
-            break;
-        case RepeatMode::RepeatPlayList:
-            state = RepeatMode::NoRepeat;
-            break;
+            case RepeatMode::NoRepeat:
+                state = RepeatMode::RepeatSong;
+                break;
+            case RepeatMode::RepeatSong:
+                state = RepeatMode::LoopSong;
+                break;
+            case RepeatMode::LoopSong:
+                state = RepeatMode::RepeatPlayList;
+                break;
+            case RepeatMode::RepeatPlayList:
+                state = RepeatMode::NoRepeat;
+                break;
         }
         return state;
     }
@@ -124,21 +79,21 @@ namespace ModPlugPlayer {
 
     inline InterpolationFilter& operator++(InterpolationFilter& state, int) {
         switch(state) {
-        case InterpolationFilter::Internal:
-            state = InterpolationFilter::NoInterpolation;
-            break;
-        case InterpolationFilter::NoInterpolation:
-            state = InterpolationFilter::LinearInterpolation;
-            break;
-        case InterpolationFilter::LinearInterpolation:
-            state = InterpolationFilter::CubicInterpolation;
-            break;
-        case InterpolationFilter::CubicInterpolation:
-            state = InterpolationFilter::WindowedSincWith8Taps;
-            break;
-        case InterpolationFilter::WindowedSincWith8Taps:
-            state = InterpolationFilter::Internal;
-            break;
+            case InterpolationFilter::Internal:
+                state = InterpolationFilter::NoInterpolation;
+                break;
+            case InterpolationFilter::NoInterpolation:
+                state = InterpolationFilter::LinearInterpolation;
+                break;
+            case InterpolationFilter::LinearInterpolation:
+                state = InterpolationFilter::CubicInterpolation;
+                break;
+            case InterpolationFilter::CubicInterpolation:
+                state = InterpolationFilter::WindowedSincWith8Taps;
+                break;
+            case InterpolationFilter::WindowedSincWith8Taps:
+                state = InterpolationFilter::Internal;
+                break;
         }
         return state;
     }
@@ -168,25 +123,9 @@ namespace ModPlugPlayer {
             default:
                 state = AmigaFilter::DisablePaulaEmulation;
                 break;
-            }
+        }
         return state;
     }
-
-
-    enum class WindowFunction :int {
-        None = 0,
-        HanningWindow = 1,
-        HammingWindow = 2,
-        BlackmanWindow = 3
-    };
-
-    enum class Stage {
-        PreAlpha,
-        Alpha,
-        Beta,
-        ReleaseCandidate,
-        StableRelease
-    };
 
     enum class PlayingMode {
         Song,
@@ -200,93 +139,4 @@ namespace ModPlugPlayer {
         GeneralPurposePlugIn,
         VisualizationPlugIn
     };
-
-    enum class AFormat
-    {
-        FMT_U8, FMT_S8, FMT_U16_LE, FMT_U16_BE, FMT_U16_NE, FMT_S16_LE, FMT_S16_BE, FMT_S16_NE
-    };
-
-    struct Version {
-        unsigned int major = 0;
-        unsigned int minor = 0;
-        unsigned int release = 0;
-        unsigned int patch = 0;
-        Stage stage = Stage::StableRelease;
-        unsigned int stageRevision = 0;
-
-        inline std::string prettyString();
-        inline std::string technicalString();
-    };
-}
-
-inline std::string ModPlugPlayer::Version::technicalString() {
-    std::ostringstream version;
-    version << major << "." << minor;
-
-    if (release != 0 || patch != 0) {
-        version << "." << release;
-    }
-
-    if (patch != 0) {
-        version << "." << patch;
-    }
-
-    switch (stage) {
-        case Stage::PreAlpha:
-            version << "-pre-alpha";
-        break;
-        case Stage::Alpha:
-            version << "-alpha";
-        break;
-        case Stage::Beta:
-            version << "-beta";
-        break;
-        case Stage::ReleaseCandidate:
-            version << "-rc";
-        break;
-        case Stage::StableRelease:
-            break;
-        default:
-            break;
-    }
-
-    if (stage != Stage::StableRelease && stageRevision != 0)
-        version << "." << stageRevision;
-
-    return version.str();
-}
-
-inline std::string ModPlugPlayer::Version::prettyString() {
-    std::ostringstream version;
-    version << major << "." << minor;
-
-    if (release != 0 || patch != 0) {
-        version << "." << release;
-    }
-
-    if (patch != 0) {
-        version << "." << patch;
-    }
-
-    switch (stage) {
-        case Stage::PreAlpha:
-            version << " Pre-Alpha";
-        break;
-        case Stage::Alpha:
-            version << " Alpha";
-        break;
-        case Stage::Beta:
-            version << " Beta";
-        break;
-        case Stage::ReleaseCandidate:
-            version << " Release Candidate";
-        break;
-        default:
-        break;
-    }
-
-    if (stage != Stage::StableRelease && stageRevision != 0)
-        version << " " << stageRevision;
-
-    return version.str();
 }
